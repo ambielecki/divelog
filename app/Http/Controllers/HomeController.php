a05b3bdd5787d20a7680bff4aa93e3cfbadae9e2
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2019-01-16 13:19:45 -0500

    started moving away from mongo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use App\BlogPage;
 use App\Image;
 use App\ImageFolder;
-use App\MongoPage;
+use App\Page;
 use Illuminate\Http\Request;
 use Session;
 
 class HomeController extends Controller
 {
     public function getHome() {
-        $page = MongoPage::where('name', '=', 'home')->where('active', '=', true)->orderBy('created_at', 'DESC')->first();
-        $image_hero = $page->image_hero ? Image::with('image_folder') ->find($page->image_hero) : '';
+        $page = Page::where('name', '=', 'home')
+            ->where('active', '=', true)
+            ->orderBy('created_at', 'DESC')
+            ->first();
+
+        $image_hero = $page->image_hero ? Image::with('image_folder')->find($page->image_hero) : '';
         $images_carousel = $page->images_carousel ? Image::with('image_folder')->findMany($page->images_carousel) : [];
         $images_single = $page->images_single ? Image::with('image_folder')->findMany($page->images_single) : [];
         $posts = BlogPage::where('is_active', '=', true)->orderBy('created_at', 'DESC')->limit(5)->get();
@@ -33,9 +37,12 @@ class HomeController extends Controller
     }
 
     public function getEditHome() {
-        $page = MongoPage::where('name', '=', 'home')->where('active', '=', true)->orderBy('created_at', 'DESC')->first();
+        $page = Page::where('slug', '=', 'home')
+            ->where('active', '=', true)
+            ->orderBy('created_at', 'DESC')
+            ->first();
         if (!$page) {
-            $page = new MongoPage();
+            $page = new Page();
         }
         $images = ImageFolder::where('name', '=', 'home')->with('images')->first();
         $hero_images = ImageFolder::where('name', '=', 'hero')->with('images')->first();
@@ -48,27 +55,27 @@ class HomeController extends Controller
     }
 
     public function postEditHome(Request $request) {
-        $previousPages = MongoPage::where('name', '=', 'home')->where('active', '=', true)->get();
-        foreach ($previousPages as $previousPage) {
-            $previousPage->active = false;
-            $previousPage->save();
-        }
-        $page = new MongoPage();
-        $page->name = 'home';
-        $page->title = $request->input('title');
-        $page->content = $request->input('content');
-        if ($request->input('hero_title') != "") {
-            $page->hero_title = $request->input('hero_title');
-        }
-        $page->image_hero = $request->input('image_hero');
-        $page->images_single = $request->input('image_single');
-        if ($request->input('carousel_title') != "") {
-            $page->carousel_title = $request->input('carousel_title');
-        }
-        $page->images_carousel = $request->input('image_carousel');
+        Page::where('name', '=', 'home')
+            ->where('active', '=', true)
+            ->update(['active' => false]);
+
+        $page = new Page();
+        $page->slug = 'home';
         $page->active = $request->input('active') ? true : false;
+        $page->data = [
+            'name' => 'home',
+            'title' => $request->input('title'),
+            'content' => $request->input('content'),
+            'hero_title' => $request->input('hero_title') ?: null,
+            'image_hero' => $request->input('image_hero'),
+            'images_single' => $request->input('image_single'),
+            'carousel_title' => $request->input('carousel_title') ?: null,
+            'images_carousel' => $request->input('image_carousel'),
+        ];
+
         $page->save();
         Session::flash('flash_success', 'Home Page Edit Successful');
+
         return redirect()->route('home_edit');
     }
 }
